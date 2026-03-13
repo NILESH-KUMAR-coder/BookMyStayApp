28d23fe919a7dfe1f7e7b0021441d1b374f60baa
CodeWithOmG <omg720944@gmail.com>
2026-03-13 15:28:16 +0530

    UC1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,9 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking System");
+        System.out.println("Application: Hotel Booking System (Version: v1.0)");
+
+    }
+}
