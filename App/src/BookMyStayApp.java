595543fc8d7e8ebef91253b9a6797bfc36939cf5
CodeWithOmG <omg720944@gmail.com>
2026-03-23 14:49:53 +0530

    UC5

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,104 +1,43 @@
-import java.util.HashMap;
+import java.util.*;
+
+class BookingRequest {
+    private String guestName;
+    private String roomType;
+
+    public BookingRequest(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() { return guestName; }
+    public String getRoomType() { return roomType; }
+}
+
+class BookingQueue {
+    private Queue<BookingRequest> requestQueue = new LinkedList<>();
+
+    public void addRequest(BookingRequest request) {
+        requestQueue.add(request);
+    }
+
+    public void processRequests() {
+        System.out.println("Booking Request Queue");
+        while (!requestQueue.isEmpty()) {
+            BookingRequest request = requestQueue.poll();
+            System.out.println("Processing booking for Guest: " + request.getGuestName() +
+                    ", Room Type: " + request.getRoomType());
+        }
+    }
+}
 
 public class BookMyStayApp {
-
     public static void main(String[] args) {
+        BookingQueue queue = new BookingQueue();
 
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
+        queue.addRequest(new BookingRequest("Om", "Single"));
+        queue.addRequest(new BookingRequest("Shubham", "Double"));
+        queue.addRequest(new BookingRequest("Shreya", "Suite"));
 
-        RoomInventory inventory = new RoomInventory();
-
-        System.out.println("Room Details");
-        System.out.println();
-
-        single.displayDetails();
-        System.out.println("Available: " + inventory.getAvailability(single.roomType));
-        System.out.println();
-
-        doubleRoom.displayDetails();
-        System.out.println("Available: " + inventory.getAvailability(doubleRoom.roomType));
-        System.out.println();
-
-        suite.displayDetails();
-        System.out.println("Available: " + inventory.getAvailability(suite.roomType));
-        System.out.println();
-
-        System.out.println("Updating availability...");
-        inventory.updateAvailability("Single Room", 4);
-
-        System.out.println();
-        System.out.println("Current Inventory");
-        inventory.displayInventory();
-    }
-}
-
-abstract class Room {
-
-    protected String roomType;
-    protected int beds;
-    protected int size;
-    protected double price;
-
-    public Room(String roomType, int beds, int size, double price) {
-        this.roomType = roomType;
-        this.beds = beds;
-        this.size = size;
-        this.price = price;
-    }
-
-    public void displayDetails() {
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Beds: " + beds);
-        System.out.println("Size: " + size + " sqm");
-        System.out.println("Price: $" + price);
-    }
-}
-
-class SingleRoom extends Room {
-
-    public SingleRoom() {
-        super("Single Room", 1, 20, 100);
-    }
-}
-
-class DoubleRoom extends Room {
-
-    public DoubleRoom() {
-        super("Double Room", 2, 30, 150);
-    }
-}
-
-class SuiteRoom extends Room {
-
-    public SuiteRoom() {
-        super("Suite Room", 3, 50, 300);
-    }
-}
-
-class RoomInventory {
-
-    private HashMap<String, Integer> inventory;
-
-    public RoomInventory() {
-        inventory = new HashMap<>();
-        inventory.put("Single Room", 5);
-        inventory.put("Double Room", 3);
-        inventory.put("Suite Room", 2);
-    }
-
-    public int getAvailability(String roomType) {
-        return inventory.getOrDefault(roomType, 0);
-    }
-
-    public void updateAvailability(String roomType, int count) {
-        inventory.put(roomType, count);
-    }
-
-    public void displayInventory() {
-        for (String roomType : inventory.keySet()) {
-            System.out.println(roomType + " Available: " + inventory.get(roomType));
-        }
+        queue.processRequests();
     }
 }
